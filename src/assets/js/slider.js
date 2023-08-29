9aec5f9387f5054039ce63cafdaf418bf32647b7
Ri4yy <maksimov.1913@list.ru>
2023-08-29 17:05:47 +0300

    updateCard

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -12,46 +12,91 @@ const swiperMain = new Swiper('.swiperMain', {
     },
 });
 
+$(function () {
+  for (let e = 0; e < 100; e++) new Swiper(".swiperProduct" + e, {
+    spaceBetween: 40,
+    enabled: true,
 
-const swiperProduct = new Swiper('.swiperProduct', {
-  spaceBetween: 40,
-  enabled: true,
-
-  breakpoints: {
-    320: {
-      allowTouchMove: true,
-      slidesPerView: 1,
-      grid: {
-        rows: 1,
-        fill: "row",
+    breakpoints: {
+      320: {
+        allowTouchMove: true,
+        slidesPerView: 1,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      630: {
+        allowTouchMove: true,
+        slidesPerView: 2,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      1024: {
+        allowTouchMove: true,
+        slidesPerView: 3,
+        grid: {
+          rows: 2,
+          fill: "row",
+        }
+      },
+      1280: {
+        allowTouchMove: true,
+        slidesPerView: 4,
+        grid: {
+          rows: 4,
+          fill: "row",
+        }
       }
     },
-    630: {
-      allowTouchMove: true,
-      slidesPerView: 2,
-      grid: {
-        rows: 1,
-        fill: "row",
-      }
+    
+    pagination: {
+      el: '.swiper-pagination-product' + e,
     },
-    1024: {
-      allowTouchMove: true,
-      slidesPerView: 3,
-      grid: {
-        rows: 2,
-        fill: "row",
-      }
-    },
-    1280: {
-      allowTouchMove: true,
-      slidesPerView: 4,
-      grid: {
-        rows: 4,
-        fill: "row",
-      }
-    }
-  },
-  pagination: {
-    el: '.swiper-pagination-product',
-  },
+  });
 });
+
+// const swiperProduct = new Swiper('.swiperProduct', {
+  // spaceBetween: 40,
+  // enabled: true,
+
+//   breakpoints: {
+//     320: {
+//       allowTouchMove: true,
+//       slidesPerView: 1,
+//       grid: {
+//         rows: 1,
+//         fill: "row",
+//       }
+//     },
+//     630: {
+//       allowTouchMove: true,
+//       slidesPerView: 2,
+//       grid: {
+//         rows: 1,
+//         fill: "row",
+//       }
+//     },
+//     1024: {
+//       allowTouchMove: true,
+//       slidesPerView: 3,
+//       grid: {
+//         rows: 2,
+//         fill: "row",
+//       }
+//     },
+//     1280: {
+//       allowTouchMove: true,
+//       slidesPerView: 4,
+//       grid: {
+//         rows: 4,
+//         fill: "row",
+//       }
+//     }
+//   },
+//   pagination: {
+//     el: '.swiper-pagination-product',
+//   },
+// });
